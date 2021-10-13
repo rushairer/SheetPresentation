6b4f8669eb403b3a751584377e40a99491a729d5
Abenx <5195693+rushairer@users.noreply.github.com>
2021-10-13 23:52:50 +0800

    Initial Commit

---
+++ Shared/SheetPresentationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetPresentationDemoApp.swift
+//  Shared
+//
+//  Created by Abenx on 2021/10/13.
+//
+
+import SwiftUI
+
+@main
+struct SheetPresentationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
